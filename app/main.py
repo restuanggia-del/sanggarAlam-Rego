b165e1f776e86068a632edf4af7671bd9691c1bf
restuanggia-del <241223303+restuanggia-del@users.noreply.github.com>
2026-02-07 21:42:02 +0700

    estimasi harga

--- app/main.py
+++ app/main.py
@@ -20,18 +20,45 @@ model_pekerja = joblib.load("model_pekerja.pkl")
 encoder_jenis = joblib.load("encoder_jenis_proyek.pkl")
 encoder_cuaca = joblib.load("encoder_cuaca.pkl")
 
+HARGA_MATERIAL = {
+    "kolam": 500_000,
+    "taman": 300_000,
+    "gazebo": 700_000
+}
+
+UPAH_HARIAN = 150_000
+TARIF_KM = 5_000
+
+FAKTOR_DETAIL = {
+    1: 1.0,
+    2: 1.1,
+    3: 1.2,
+    4: 1.3,
+    5: 1.5
+}
+
+FAKTOR_CUACA = {
+    "cerah": 1.0,
+    "mendung": 1.05,
+    "hujan": 1.15
+}
+
 @app.get("/")
 def root():
     return {"message": "API Sanggar Alam aktif"}
 
 @app.post("/estimasi")
 def estimasi(data: dict):
+
+    jenis_proyek_asli = data["jenis_proyek"]
+    cuaca_asli = data["cuaca"]
+
     df = pd.DataFrame([data])
 
     df["jenis_proyek"] = encoder_jenis.transform(df["jenis_proyek"])
     df["cuaca"] = encoder_cuaca.transform(df["cuaca"])
 
-    fitur_dp = df[[
+    fitur_ml = df[[
         "jenis_proyek",
         "luas_m2",
         "tingkat_detail",
@@ -39,23 +66,35 @@ def estimasi(data: dict):
         "jarak_km"
     ]]
 
-    durasi = int(model_durasi.predict(fitur_dp)[0])
-    pekerja = int(model_pekerja.predict(fitur_dp)[0])
+    durasi = int(model_durasi.predict(fitur_ml)[0])
+    pekerja = int(model_pekerja.predict(fitur_ml)[0])
 
-    fitur_harga = pd.DataFrame([{
-        "jenis_proyek": df["jenis_proyek"][0],
-        "luas_m2": df["luas_m2"][0],
-        "tingkat_detail": df["tingkat_detail"][0],
-        "jumlah_pekerja": pekerja,
-        "durasi_hari": durasi,
-        "jarak_km": df["jarak_km"][0]
-    }])
+    harga_material = (
+        data["luas_m2"] *
+        HARGA_MATERIAL.get(jenis_proyek_asli, 400_000)
+    )
 
-    harga = int(model_harga.predict(fitur_harga)[0])
+    biaya_upah = pekerja * UPAH_HARIAN * durasi
+    biaya_transport = data["jarak_km"] * TARIF_KM
+
+    subtotal = harga_material + biaya_upah + biaya_transport
+
+    faktor_detail = FAKTOR_DETAIL[data["tingkat_detail"]]
+    faktor_cuaca = FAKTOR_CUACA[cuaca_asli]
+
+    estimasi_harga = int(subtotal * faktor_detail * faktor_cuaca)
 
     return {
-        "estimasi_harga": harga,
-        "estimasi_durasi_hari": durasi,
-        "estimasi_jumlah_pekerja": pekerja
+    "estimasi_harga": estimasi_harga,
+    "estimasi_durasi_hari": durasi,
+    "estimasi_jumlah_pekerja": pekerja,
+    "breakdown": {
+        "material": harga_material,
+        "upah": biaya_upah,
+        "transport": biaya_transport,
+        "subtotal": subtotal,
+        "faktor_detail": faktor_detail,
+        "faktor_cuaca": faktor_cuaca
+        }
     }
     
